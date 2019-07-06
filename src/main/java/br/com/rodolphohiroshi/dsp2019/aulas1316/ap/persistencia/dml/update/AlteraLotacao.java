60b501d7eec756041f1c3cf54469f11df6174b21
rodolphohiroshi <rodolpho.ht@hotmail.com>
2019-07-06 20:10:50 -0300

    Commit arquivos xml e json aulas1912.ap

--- src/main/java/br/com/rodolphohiroshi/dsp2019/aulas1316/ap/persistencia/dml/update/AlteraLotacao.java
+++
@@ -1,4 +0,0 @@
-package br.com.rodolphohiroshi.dsp2019.aulas1316.ap.persistencia.dml.update;
-
-public class AlteraLotacao {
-}
--- src/main/java/br/com/rodolphohiroshi/dsp2019/aulas1720/ap/persistencia/dml/update/AlteraDepartamento.java
+++
@@ -1,4 +0,0 @@
-package br.com.rodolphohiroshi.dsp2019.aulas1720.ap.persistencia.dml.update;
-
-public class AlteraDepartamento {
-}
